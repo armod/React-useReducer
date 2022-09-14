caac7485028d042f2ab3662c240aabf929839dd6
armod <armods@gmail.com>
2022-09-14 12:03:01 +0200

    cart

--- src/context.js
+++ src/context.js
@@ -7,12 +7,22 @@ const url = 'https://course-api.com/react-useReducer-cart-project'
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems)
+  const initialState = {
+    loading: false,
+    cart: cartItems,
+    total: 0,
+    amount: 0,
+  }
 
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const clearCart = () => {
+    dispatch({ type: 'CLEAR_CART' })
+  }
   return (
     <AppContext.Provider
       value={{
-        cart,
+        ...state,
+        clearCart,
       }}
     >
       {children}
